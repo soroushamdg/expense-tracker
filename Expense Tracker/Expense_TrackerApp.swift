3014517f9fcdfab651cc95d5375cf46ad2ee1ddc
soroushamdg <soroushmpour@gmail.com>
2023-01-04 13:16:01 -0500

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Soro on 2023-01-04.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
